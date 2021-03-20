93bf7a1d010422cf7559d4d7018fe7e04b025472
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-03-20 22:12:18 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+function set_lift_pos (num: number, lift: boolean) {
+    while (num != lift_angle) {
+        if (lift) {
+            lift_angle += -5
+            maqueen.servoRun(maqueen.Servos.S2, lift_angle)
+            if (lift_angle <= num) {
+                break;
+            }
+            basic.pause(100)
+        } else {
+            lift_angle += 5
+            maqueen.servoRun(maqueen.Servos.S2, lift_angle)
+            if (lift_angle >= num) {
+                break;
+            }
+            basic.pause(100)
+        }
+    }
+}
+function set_grab_pos (num: number, grab: boolean) {
+    while (num != grab_angle) {
+        if (grab) {
+            grab_angle += 5
+            maqueen.servoRun(maqueen.Servos.S1, grab_angle)
+            serial.writeValue("grab s1", grab_angle)
+            if (grab_angle >= num) {
+                break;
+            }
+            basic.pause(100)
+        } else {
+            grab_angle += -5
+            maqueen.servoRun(maqueen.Servos.S1, grab_angle)
+            if (grab_angle <= num) {
+                break;
+            }
+            basic.pause(100)
+        }
+    }
+}
+let lift_angle = 0
+let grab_angle = 0
+grab_angle = 100
+lift_angle = 100
+basic.forever(function () {
+    set_grab_pos(40, false)
+    basic.pause(1000)
+    set_lift_pos(40, true)
+    basic.pause(1000)
+    set_grab_pos(100, true)
+    basic.pause(1000)
+    set_lift_pos(100, false)
+    basic.pause(1000)
+})
